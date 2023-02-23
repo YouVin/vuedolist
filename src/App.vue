32e19f57479c76b0a3165eb3ce2974eb589b2513
YuVin <dbqls1210@gmail.com>
2023-02-23 22:44:00 +0900

    test1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted, computed, watch} from 'vue'
+
+const todo = ref([])
+const nam = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+</script>
+
+<template>
+<div>
+  Hello world
+</div>
+</template>
